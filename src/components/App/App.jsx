0da6d2e865bad068400568709f0f50e1f8543e12
Hugo Chantelauze <hugo@etamin.studio>
2023-09-15 18:40:02 +0200

    Setup

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import reactLogo from '../../assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>KASA: My first app with REACT</h1>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
