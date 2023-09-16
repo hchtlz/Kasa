41fbd718f018e73b935a02c73a292a18b2a8a4fb
Hugo Chantelauze <hugo@etamin.studio>
2023-09-16 17:32:26 +0200

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable */
+import './Header.css'
+import { useEffect, useState } from 'react';
+
+const Header = () => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname); // Obtenez le chemin de la page actuelle
+  useEffect(() => {
+    // Mettez à jour le chemin de la page actuelle lorsque l'URL change
+    const handleLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    // Écoutez les changements d'URL
+    window.addEventListener('popstate', handleLocationChange);
+
+    // Nettoyez l'écouteur d'événements lorsque le composant est démonté
+    return () => {
+      window.removeEventListener('popstate', handleLocationChange);
+    };
+  }, []);
+
+  return (
+    <header className="header">
+      <img src="../../src/assets/images/logo-header.svg" alt="logo" className="header__logo" />
+      <nav>
+        <ul className="header__menu">
+          <li className={`header__menu-item ${currentPath === '/' ? 'active' : ''}`}>
+            <a href="#home">Accueil</a>
+          </li>
+          <li className={`header__menu-item ${currentPath === '/a-propos' ? 'active' : ''}`}>
+            <a href="#about">A propos</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Data from './components/Data/Data'
+import Header from './components/Header/Header'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     <Data />
   </React.StrictMode>,
 )
